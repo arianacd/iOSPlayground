3517b4fd8e9aa8712d3ebee36b7e550fbddb9235
ariana <ariana.daney@ssfs.org>
2020-02-04 14:58:08 -0500

    Finished designing calculater

---
+++ Calculater/Calculater/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculater
+//
+//  Created by Ariana on 2/3/20.
+//  Copyright © 2020 Ariana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
