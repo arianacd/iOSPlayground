c98ac90e5c16ef231143224541cee8dea683e2ac
ariana <ariana.daney@ssfs.org>
2020-02-07 10:16:31 -0500

    Worked on calculater

---
+++ TemperatureConverter/TemperatureConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemperatureConverter
+//
+//  Created by Ariana on 2/6/20.
+//  Copyright © 2020 Ariana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
