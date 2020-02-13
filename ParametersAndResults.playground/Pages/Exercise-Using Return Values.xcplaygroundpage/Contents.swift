be436cd470d75b47af1c7c148e8a2a37a231265f
ariana <ariana.daney@ssfs.org>
2020-02-13 15:07:54 -0500

    finished parameters and results

--- ParametersAndResults.playground/Pages/Exercise-Using Return Values.xcplaygroundpage/Contents.swift
+++ ParametersAndResults.playground/Pages/Exercise-Using Return Values.xcplaygroundpage/Contents.swift
@@ -5,16 +5,13 @@
 
  The function `impossibleBeliefsCount` takes several numbers of reported unlikely incidents. It then prints the number of impossible things to be believed:
 */
-func impossibleBeliefsCount(pigsFlying: Int, frogsBecomingPrinces: Int, multipleLightningStrikes: Int) {
-    let total = pigsFlying + frogsBecomingPrinces + multipleLightningStrikes
-    print(total)
+func impossibleBeliefsCount(pigsFlying: Int, frogsBecomingPrinces: Int, multipleLightningStrikes: Int) -> Int{
+    return pigsFlying + frogsBecomingPrinces + multipleLightningStrikes
 }
 //: - callout(Exercise):Update the `impossibleBeliefsCount` function so that instead of printing the value, it returns it as an `Int`.
 //:
 //: `impossibleThingsPhrase` creates a phrase using string interpolation:
-func impossibleThingsPhrase() -> String {
-    let numberOfImpossibleThings = 10
-    let meal = "teatime"
+func impossibleThingsPhrase(numberOfImpossibleThings: Int, meal: String) -> String {
     return "Why, I've believed as many as \(numberOfImpossibleThings) before \(meal)"
 }
 //: - callout(Exercise): Update the `impossibleThingsPhrase` function so that, instead of using its two internal constants, it takes two arguments: `numberOfImpossibleThings` as an `Int` and `meal` as a `String`.
@@ -22,8 +19,8 @@ func impossibleThingsPhrase() -> String {
 //: Now you have two functions that take parameters and return values.
 //: - callout(Exercise): Call `impossibleBeliefsCount` and store the result in a constant.\
 //:Call `impossibleThingsPhrase`, passing in the result of `impossibleBeliefsCount` as one of the arguments.
-
-
+let beliefs = impossibleBeliefsCount(pigsFlying: 2, frogsBecomingPrinces: 2, multipleLightningStrikes: 2)
+impossibleThingsPhrase(numberOfImpossibleThings: beliefs, meal: "teatime")
 
 
 
