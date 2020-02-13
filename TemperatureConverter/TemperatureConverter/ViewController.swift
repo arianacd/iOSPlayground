6a74141d4e7fed092d0c4e534241e415ce93e405
ariana <ariana.daney@ssfs.org>
2020-02-13 14:48:07 -0500

    worked on traffic light

--- TemperatureConverter/TemperatureConverter/ViewController.swift
+++ TemperatureConverter/TemperatureConverter/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        view.addGestureRecognizer(tap)
     }
 
 
---
+++ TrafficLights/TrafficLights/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TrafficLights
+//
+//  Created by Ariana on 2/13/20.
+//  Copyright © 2020 Ariana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func returnToFirstScreen(unwindSegue: UIStoryboardSegue){
+        
+    }
+
+}
+
