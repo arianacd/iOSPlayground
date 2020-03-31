af7e66454269d2b9fbff4fba61cdb93ddf8b2d84
Ariana Daney <arianadaney@Arianas-Air.fios-router.home>
2020-03-31 14:10:40 -0400

    added buttons with letters and connected to view controller and list of words

--- Apple Pie/Apple Pie/ViewController.swift
+++ Apple Pie/Apple Pie/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+var listOfWords = ["chocolate", "rhythm", "jazz", "knight", "giraffe", "abject", "jewelry"]
+let incorrectMovesAllowed = 7
+var totalWins = 0
+var totalLosses = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var treeImageView: UIImageView!
+    
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
 }
-
