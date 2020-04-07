cbacfdced9c8a48a5be89d2f0b507eb32abab3e3
Ariana Daney <arianadaney@Arianas-Air.fios-router.home>
2020-04-07 10:08:01 -0400

    created the project and added a button to open a new view controller

---
+++ FindYourColors/FindYourColors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindYourColors
+//
+//  Created by Ariana Daney on 4/7/20.
+//  Copyright © 2020 Ariana Daney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
