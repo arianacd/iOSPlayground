e2e73caadb8f131b1f055e018e03973e9e4b5e85
Ariana Daney <arianadaney@Arianas-Air.fios-router.home>
2020-04-08 11:38:23 -0400

    added table view controller and palette swift file

---
+++ FindYourColors/FindYourColors/Palette.swift
@@ -0,0 +1,16 @@
+//
+//  Palette.swift
+//  FindYourColors
+//
+//  Created by Ariana Daney on 4/8/20.
+//  Copyright © 2020 Ariana Daney. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Palette {
+    var name: String
+    var colors: [UIColor]
+    
+}
